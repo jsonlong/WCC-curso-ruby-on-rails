39d901a9fb81ccade383109054ac463c33e28341
Christian Camacho <jsonlong05d97@gmail.com>
2017-07-01 10:02:24 -0700

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pivotalify::Application.config.secret_key_base = '2cea403e88744c76671579afdea6edbfbc5c3326acbf19cba47c284d374232ed67ed029afb45af3b7fdc1288f5e9da73867f901a8367d3522f9f3d0fd38e8d97'
